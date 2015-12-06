3384e9dad9cbd438be568deee9fc204796755a4f
test <test@Eddie-PC>
2015-12-06 19:58:32 +0800

    Pong 1206 1958

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -16,7 +16,7 @@ public class TestGit {
      */
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("Pong Work");
+        System.out.println("Pong Erro");
     }
     
 }
