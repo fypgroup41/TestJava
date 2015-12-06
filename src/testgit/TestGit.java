eb939f9e983f89973373e4b84d9691a06f04b991
Eddie <j23349@gmail.com>
2015-12-06 20:13:45 +0800

    Eddie 1206 2013

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -16,7 +16,7 @@ public class TestGit {
      */
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("Pong Erro");
+        System.out.println("Pong Error");
     }
     
 }
