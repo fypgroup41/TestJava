5e644e6d5731a4e8616555a43551763db348accb
Eddie <j23349@gmail.com>
2015-12-07 16:14:53 +0800

    Eddie 1207 1614

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -17,6 +17,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("Pong Error");
+           System.out.println("Eddie Error");
     }
     
 }
